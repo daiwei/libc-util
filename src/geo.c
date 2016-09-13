c36b0e351489fa329e3903e9f0895610bddd5b63
daiwei <dai.wei@outlook.com>
2016-09-13 20:50:43 +0800

    goe

--- src/geo.c
+++ src/geo.c
@@ -1,5 +1,90 @@
 #include "geo.h"
 
+/**
+ * lat lng converter
+ */
+char lat_zone(double value)
+{
+    if (value > 0) {
+        return 'N';
+    } else {
+        return 'S';
+    }
+}
+
+char lng_zone(double value)
+{
+    if (value > 0) {
+        return 'E';
+    } else {
+        return 'W';
+    }
+}
+
+int lat_sign(char zone)
+{
+    if (zone == 'E') {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int lng_sign(char zone)
+{
+    if (zone == 'N') {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+double lnglat_nmea_to_double(double value, char zone)
+{
+    int sign = 1;
+
+    if (zone == 'E' || zone == 'N') {
+        sign = 1;
+    } else {
+        sign = -1;
+    }
+
+    double degree = floor(value / 100);
+    double minute = value - degree * 100;
+    return sign * (degree + minute / 60);
+}
+
+double lnglat_nmea_to_degree(double value)
+{
+    return floor(value / 100);
+}
+
+double lnglat_nmea_to_minute(double value)
+{
+    return value - floor(value / 100) * 100;
+}
+
+double lnglat_double_to_nmea(double value)
+{
+    value = value < 0 ? value * -1 : value;
+    double degree = floor(value);
+    double minute = (value - degree) * 60;
+
+    return degree * 100 + minute;
+}
+
+double lnglat_double_to_degree(double value)
+{
+    value = value < 0 ? value * -1 : value;
+    return floor(value);
+}
+
+double lnglat_double_to_minute(double value)
+{
+    value = value < 0 ? value * -1 : value;
+    return (value - floor(value)) * 60;
+}
+
 double distance(lat1, lng1, lat2, lng2)
 {
     double a, b;
--- src/geo.h
+++ src/geo.h
@@ -6,4 +6,16 @@
 #define PI                      3.14159265359
 #define EARTH_R                 6378137
 
+char lat_zone(double value);
+char lng_zone(double value);
+int lat_sign(char zone);
+int lng_sign(char zone);
+
+double lnglat_nmea_to_double(double value, char zone);
+double lnglat_nmea_to_degree(double value);
+double lnglat_nmea_to_minute(double value);
+double lnglat_double_to_nmea(double value);
+double lnglat_double_to_degree(double value);
+double lnglat_double_to_minute(double value);
+
 #endif//__LIBC_UTIL_GEO_H__
